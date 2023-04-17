f6eeffd5159f571746f9fb251b1466a173889a85
muzamil4546 <muzamilchamp14@gmail.com>
2023-04-17 22:45:51 +0500

    before adding validation

--- lib/UiScreens/SignUp_Screen.dart
+++ lib/UiScreens/SignUp_Screen.dart
@@ -9,6 +9,9 @@ class SignUp extends StatefulWidget {
 }
 
 class _SignUpState extends State<SignUp> {
+  final _formKey = GlobalKey<FormState>();
+  final emailController = TextEditingController();
+  final passwordController = TextEditingController();
   @override
   Widget build(BuildContext context) {
     return Scaffold(
@@ -18,44 +21,50 @@ class _SignUpState extends State<SignUp> {
       ),
       body: Padding(
         padding: const EdgeInsets.all(30.0),
-        child: Column(
-          // mainAxisAlignment: MainAxisAlignment.center,
-          children: [
-            Text('SignUp Screen',
-              style: TextStyle(
-                  color: Colors.black,
-                  fontSize: 30),
+        child: Form(
+          key: _formKey,
+          child: Column(
+            // mainAxisAlignment: MainAxisAlignment.center,
+            children: [
+              Text('SignUp Screen',
+                style: TextStyle(
+                    color: Colors.black,
+                    fontSize: 30),
+              ),
+            SizedBox(height: 100,),
+            TextFormField(
+              controller: emailController,
+                keyboardType: TextInputType.emailAddress,
+                decoration: InputDecoration(
+                  suffixIcon: Icon(Icons.email),
+                  hintText: 'Enter Email',
+                  labelStyle: TextStyle(
+                      color: Colors.black,fontSize: 20),
+                  border: OutlineInputBorder(
+                    borderRadius: BorderRadius.circular(20),
+                  ),
+                )
             ),
-          SizedBox(height: 100,),
-          TextFormField(
-              keyboardType: TextInputType.emailAddress,
-              decoration: InputDecoration(
-                suffixIcon: Icon(Icons.email),
-                hintText: 'Enter Email',
-                labelStyle: TextStyle(
-                    color: Colors.black,fontSize: 20),
-                border: OutlineInputBorder(
-                  borderRadius: BorderRadius.circular(20),
-                ),
-              )
-          ),
-          SizedBox(height: 20),
-          TextFormField(
-              keyboardType: TextInputType.visiblePassword,
-              obscureText: true,
-              decoration: InputDecoration(
-                suffixIcon: Icon(Icons.lock),
-                hintText: 'Enter Password',
-                labelStyle: TextStyle(
-                    color: Colors.black,fontSize: 20),
-                border: OutlineInputBorder(
-                  borderRadius: BorderRadius.circular(20),
-                ),
-              )
-          ),
-            SizedBox(height: 30,),
-            CustomButton(buttonTitle: 'Sign Up',),
-        ],),
+            SizedBox(height: 20),
+            TextFormField(
+              controller: passwordController,
+                keyboardType: TextInputType.visiblePassword,
+                obscureText: true,
+                decoration: InputDecoration(
+                  suffixIcon: Icon(Icons.lock),
+                  hintText: 'Enter Password',
+                  labelStyle: TextStyle(
+                      color: Colors.black,fontSize: 20),
+                  border: OutlineInputBorder(
+                    borderRadius: BorderRadius.circular(20),
+                  ),
+                )
+            ),
+              SizedBox(height: 30,),
+              CustomButton(buttonTitle: 'Sign Up',
+              ),
+          ],),
+        ),
       ),
     );
   }
--- lib/UiScreens/login_Screen.dart
+++ lib/UiScreens/login_Screen.dart
@@ -29,68 +29,67 @@ class _LoginScreenState extends State<LoginScreen> {
       backgroundColor: Colors.white,
       body: Padding(
         padding: const EdgeInsets.all(30.0),
-        child: Column(
-            children: [
-              // Form(                                             form key for validation
-              //     key: _formKey,
-              //     child: Column()
-              // ),
-              Text('Login Screen',
-                  style: TextStyle(
-                      color: Colors.black,
-                      fontSize: 30),
+        child: Form(
+          key: _formKey,
+          child: Column(
+              children: [
+                Text('Login Screen',
+                    style: TextStyle(
+                        color: Colors.black,
+                        fontSize: 30),
+                  ),
+                SizedBox(height: 100),
+                TextFormField(
+                  controller: emailController,
+                    keyboardType: TextInputType.emailAddress,
+                    decoration: InputDecoration(
+                  suffixIcon: Icon(Icons.email),
+                    hintText: 'Enter Email',
+                    labelStyle: TextStyle(
+                        color: Colors.black,fontSize: 20),
+                  border: OutlineInputBorder(
+                      borderRadius: BorderRadius.circular(20),
+                  ),
+                )
                 ),
-              SizedBox(height: 100),
-              TextFormField(
-                controller: emailController,
-                  keyboardType: TextInputType.emailAddress,
-                  decoration: InputDecoration(
-                suffixIcon: Icon(Icons.email),
-                  hintText: 'Enter Email',
+                SizedBox(height: 20),
+                TextFormField(
+                  controller: passwordController,
+                  keyboardType: TextInputType.visiblePassword,
+                    obscureText: true,
+                    decoration: InputDecoration(
+                  suffixIcon: Icon(Icons.lock),
+                  hintText: 'Enter Password',
                   labelStyle: TextStyle(
                       color: Colors.black,fontSize: 20),
-                border: OutlineInputBorder(
+                  border: OutlineInputBorder(
                     borderRadius: BorderRadius.circular(20),
+                  ),
+                )
                 ),
-              )
-              ),
-              SizedBox(height: 20),
-              TextFormField(
-                controller: passwordController,
-                keyboardType: TextInputType.visiblePassword,
-                  obscureText: true,
-                  decoration: InputDecoration(
-                suffixIcon: Icon(Icons.lock),
-                hintText: 'Enter Password',
-                labelStyle: TextStyle(
-                    color: Colors.black,fontSize: 20),
-                border: OutlineInputBorder(
-                  borderRadius: BorderRadius.circular(20),
-                ),
-              )
-              ),
-              SizedBox(height: 50),
-              CustomButton(),
-              SizedBox(height: 30,),
-              Text.rich(
-               TextSpan(
-                   text: 'If you dont have an account?',style: TextStyle(fontSize: 15),
-                   children: [TextSpan(
-                       text: ' Sign Up',
-                       recognizer: new TapGestureRecognizer()
-                         ..onTap = () =>
-                         {
-                           Navigator.push(context, MaterialPageRoute(
-                               builder: (context) => SignUp()),
-                           )
-                         },
-                       style: TextStyle(
-                     color: Colors.black,fontWeight: FontWeight.bold,
-                         fontSize: 20,)
-                   )
-                   ]),
-              )
-            ],
+                SizedBox(height: 50),
+                CustomButton(),
+                SizedBox(height: 30,),
+                Text.rich(
+                 TextSpan(
+                     text: 'If you dont have an account?',style: TextStyle(fontSize: 15),
+                     children: [TextSpan(
+                         text: ' Sign Up',
+                         recognizer: new TapGestureRecognizer()
+                           ..onTap = () =>
+                           {
+                             Navigator.push(context, MaterialPageRoute(
+                                 builder: (context) => SignUp()),
+                             )
+                           },
+                         style: TextStyle(
+                       color: Colors.black,fontWeight: FontWeight.bold,
+                           fontSize: 20,)
+                     )
+                     ]),
+                )
+              ],
+          ),
         ),
       ),
     );
