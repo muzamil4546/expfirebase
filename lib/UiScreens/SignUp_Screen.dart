fe728af7fab2a3833c7c3c4458c8338ddb338192
muzamil4546 <muzamilchamp14@gmail.com>
2023-04-26 21:51:16 +0500

    after adding firebase

--- lib/UiScreens/SignUp_Screen.dart
+++ lib/UiScreens/SignUp_Screen.dart
@@ -1,5 +1,11 @@
+import 'package:expfirebase/utils/utils.dart';
 import 'package:expfirebase/widgets.dart';
+import 'package:firebase_auth/firebase_auth.dart';
+import 'package:flutter/gestures.dart';
 import 'package:flutter/material.dart';
+import 'package:firebase_auth/firebase_auth.dart';
+
+import 'login_Screen.dart';
 
 class SignUp extends StatefulWidget {
   const SignUp({Key? key}) : super(key: key);
@@ -9,9 +15,12 @@ class SignUp extends StatefulWidget {
 }
 
 class _SignUpState extends State<SignUp> {
+  bool loading = false;
   final _formKey = GlobalKey<FormState>();
   final emailController = TextEditingController();
   final passwordController = TextEditingController();
+
+  FirebaseAuth _auth = FirebaseAuth.instance;
   @override
   Widget build(BuildContext context) {
     return Scaffold(
@@ -23,47 +32,106 @@ class _SignUpState extends State<SignUp> {
         padding: const EdgeInsets.all(30.0),
         child: Form(
           key: _formKey,
-          child: Column(
-            // mainAxisAlignment: MainAxisAlignment.center,
-            children: [
-              Text('SignUp Screen',
-                style: TextStyle(
-                    color: Colors.black,
-                    fontSize: 30),
-              ),
-            SizedBox(height: 100,),
-            TextFormField(
-              controller: emailController,
-                keyboardType: TextInputType.emailAddress,
-                decoration: InputDecoration(
-                  suffixIcon: Icon(Icons.email),
-                  hintText: 'Enter Email',
-                  labelStyle: TextStyle(
-                      color: Colors.black,fontSize: 20),
-                  border: OutlineInputBorder(
-                    borderRadius: BorderRadius.circular(20),
+          child: SingleChildScrollView(
+            child: Column(
+              children: [
+                Text('SignUp Screen',
+                  style: TextStyle(
+                      color: Colors.black,
+                      fontSize: 30),
+                ),
+              SizedBox(height: 100),
+              TextFormField(
+                controller: emailController,
+                  keyboardType: TextInputType.emailAddress,
+                  decoration: InputDecoration(
+                    suffixIcon: Icon(Icons.email),
+                    hintText: 'Enter Email',
+                    labelStyle: TextStyle(
+                        color: Colors.black,fontSize: 20),
+                    border: OutlineInputBorder(
+                      borderRadius: BorderRadius.circular(20),
+                    ),
                   ),
-                )
-            ),
-            SizedBox(height: 20),
-            TextFormField(
-              controller: passwordController,
-                keyboardType: TextInputType.visiblePassword,
-                obscureText: true,
-                decoration: InputDecoration(
-                  suffixIcon: Icon(Icons.lock),
-                  hintText: 'Enter Password',
-                  labelStyle: TextStyle(
-                      color: Colors.black,fontSize: 20),
-                  border: OutlineInputBorder(
-                    borderRadius: BorderRadius.circular(20),
-                  ),
-                )
-            ),
-              SizedBox(height: 30,),
-              CustomButton(buttonTitle: 'Sign Up',
+                validator: (value){
+                  if(value!.isEmpty){
+                    return 'Enter Email';
+                  }
+                  return null;
+                },
               ),
-          ],),
+              SizedBox(height: 20),
+              TextFormField(
+                controller: passwordController,
+                  keyboardType: TextInputType.visiblePassword,
+                  obscureText: true,
+                  decoration: InputDecoration(
+                    suffixIcon: Icon(Icons.lock),
+                    hintText: 'Enter Password',
+                    labelStyle: TextStyle(
+                        color: Colors.black,fontSize: 20),
+                    border: OutlineInputBorder(
+                      borderRadius: BorderRadius.circular(20),
+                    ),
+                  ),
+                validator: (value){
+                  if(value!.isEmpty){
+                    return 'Enter Password';
+                  }
+                  return null;
+                },
+              ),
+                SizedBox(height: 30),
+            ElevatedButton(
+              onPressed: (){
+                  if(_formKey.currentState!.validate()){
+                    setState(() {
+                      loading = true;
+                    });
+                    _auth.createUserWithEmailAndPassword(
+                        email: emailController.text.toString(),
+                        password: passwordController.text.toString()).then((value){
+                      setState(() {
+                        loading = false;
+                      });
+                    }).onError((error, stackTrace){
+                      setState(() {
+                        loading = false;
+                      });
+                      Utils().toastMessage(error.toString());
+                    });
+                  }
+              },
+              child: Text('Sign Up',
+                  style: TextStyle(
+                      fontSize: 20,
+                      color: Colors.white)
+              ),
+              style: ButtonStyle(
+                  backgroundColor: MaterialStatePropertyAll(Colors.purple)
+              ),
+            ),
+                SizedBox(height: 30,),
+                Text.rich(
+                  TextSpan(
+                      text: 'If you dont have an account?',style: TextStyle(fontSize: 15),
+                      children: [TextSpan(
+                          text: ' Login',
+                          recognizer: new TapGestureRecognizer()
+                            ..onTap = () =>
+                            {
+                              Navigator.push(context, MaterialPageRoute(
+                                  builder: (context) => LoginScreen()),
+                              )
+                            },
+                          style: TextStyle(
+                            color: Colors.purple,fontWeight: FontWeight.bold,
+                            fontSize: 21,)
+                      )
+                      ]),
+                )
+            ],),
+          ),
         ),
       ),
     );
--- lib/UiScreens/login_Screen.dart
+++ lib/UiScreens/login_Screen.dart
@@ -1,3 +1,5 @@
+import 'package:expfirebase/utils/utils.dart';
+import 'package:firebase_auth/firebase_auth.dart';
 import 'package:flutter/gestures.dart';
 import 'package:flutter/material.dart';
 
@@ -15,6 +17,18 @@ class _LoginScreenState extends State<LoginScreen> {
   final _formKey = GlobalKey<FormState>();
   final emailController = TextEditingController();
   final passwordController = TextEditingController();
+
+  FirebaseAuth _auth = FirebaseAuth.instance;
+
+  void login(){
+    _auth.signInWithEmailAndPassword(email: emailController.text.toString(),
+        password: passwordController.text.toString()).then((value){
+          Utils().toastMessage(value.user!.email.toString());
+    }).onError((error, stackTrace){
+      debugPrint(error.toString());
+      Utils().toastMessage(error.toString());
+    });
+  }
   @override
   Widget build(BuildContext context) {
     return Scaffold(
@@ -31,64 +45,92 @@ class _LoginScreenState extends State<LoginScreen> {
         padding: const EdgeInsets.all(30.0),
         child: Form(
           key: _formKey,
-          child: Column(
-              children: [
-                Text('Login Screen',
-                    style: TextStyle(
-                        color: Colors.black,
-                        fontSize: 30),
+          child: SingleChildScrollView(
+            child: Column(
+                children: [
+                  Text('Login Screen',
+                      style: TextStyle(
+                          color: Colors.black,
+                          fontSize: 30),
+                    ),
+                  SizedBox(height: 100),
+                  TextFormField(
+                    controller: emailController,
+                      keyboardType: TextInputType.emailAddress,
+                      decoration: InputDecoration(
+                    suffixIcon: Icon(Icons.email),
+                      hintText: 'Enter Email',
+                      labelStyle: TextStyle(
+                          color: Colors.black,fontSize: 20),
+                    border: OutlineInputBorder(
+                        borderRadius: BorderRadius.circular(20),
+                    ),
                   ),
-                SizedBox(height: 100),
-                TextFormField(
-                  controller: emailController,
-                    keyboardType: TextInputType.emailAddress,
-                    decoration: InputDecoration(
-                  suffixIcon: Icon(Icons.email),
-                    hintText: 'Enter Email',
+                    validator: (value){
+                      if(value!.isEmpty){
+                        return 'Enter Email';
+                      }
+                      return null;
+                    },
+                  ),
+                  SizedBox(height: 20),
+                  TextFormField(
+                    controller: passwordController,
+                    keyboardType: TextInputType.visiblePassword,
+                      obscureText: true,
+                      decoration: InputDecoration(
+                    suffixIcon: Icon(Icons.lock),
+                    hintText: 'Enter Password',
                     labelStyle: TextStyle(
                         color: Colors.black,fontSize: 20),
-                  border: OutlineInputBorder(
+                    border: OutlineInputBorder(
                       borderRadius: BorderRadius.circular(20),
+                    ),
                   ),
-                )
-                ),
-                SizedBox(height: 20),
-                TextFormField(
-                  controller: passwordController,
-                  keyboardType: TextInputType.visiblePassword,
-                    obscureText: true,
-                    decoration: InputDecoration(
-                  suffixIcon: Icon(Icons.lock),
-                  hintText: 'Enter Password',
-                  labelStyle: TextStyle(
-                      color: Colors.black,fontSize: 20),
-                  border: OutlineInputBorder(
-                    borderRadius: BorderRadius.circular(20),
+                    validator: (value){
+                      if(value!.isEmpty){
+                        return 'Enter Password';
+                      }
+                      return null;
+                    },
                   ),
-                )
-                ),
-                SizedBox(height: 50),
-                CustomButton(),
-                SizedBox(height: 30,),
-                Text.rich(
-                 TextSpan(
-                     text: 'If you dont have an account?',style: TextStyle(fontSize: 15),
-                     children: [TextSpan(
-                         text: ' Sign Up',
-                         recognizer: new TapGestureRecognizer()
-                           ..onTap = () =>
-                           {
-                             Navigator.push(context, MaterialPageRoute(
-                                 builder: (context) => SignUp()),
-                             )
-                           },
-                         style: TextStyle(
-                       color: Colors.black,fontWeight: FontWeight.bold,
-                           fontSize: 20,)
-                     )
-                     ]),
-                )
-              ],
+                  SizedBox(height: 50),
+            ElevatedButton(
+              onPressed: (){
+                if(_formKey.currentState!.validate()){
+                  login();
+                }
+              },
+              child: Text('Login',
+                  style: TextStyle(
+                      fontSize: 20,
+                      color: Colors.white)
+              ),
+              style: ButtonStyle(
+                  backgroundColor: MaterialStatePropertyAll(Colors.purple)
+              ),
+            ),
+                  SizedBox(height: 30,),
+                  Text.rich(
+                   TextSpan(
+                       text: 'If you dont have an account?',style: TextStyle(fontSize: 15),
+                       children: [TextSpan(
+                           text: ' Sign Up',
+                           recognizer: new TapGestureRecognizer()
+                             ..onTap = () =>
+                             {
+                               Navigator.push(context, MaterialPageRoute(
+                                   builder: (context) => SignUp()),
+                               )
+                             },
+                           style: TextStyle(
+                         color: Colors.purple,fontWeight: FontWeight.bold,
+                             fontSize: 20,)
+                       )
+                       ]),
+                  )
+                ],
+            ),
           ),
         ),
       ),
